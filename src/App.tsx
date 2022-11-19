6ed3545635304a2132f42c7f78af808601d487ff
jefferson.morais <jefferson.morais@willbank.com.br>
2022-11-19 17:56:50 -0300

    chore: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import {  RouterProvider } from "react-router-dom";
+import {router} from './routes'
 
 export const App = () => {
   return (
     <div className="App">
-  
+      <RouterProvider router={router}/>
     </div>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {}
+import { createBrowserRouter } from "react-router-dom";
+  
+export const router = createBrowserRouter([
+    {
+        path: "*",
+        element: <div>Hello World!</div>,
+      },
+    {
+      path: "/dev",
+      element: <div>Hello Jefferson!</div>,
+    },
+  ]);
